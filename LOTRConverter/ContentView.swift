016f4bdb48dc304c8a3e50a0432c2c5b7cf55e58
deepak <deepakkumardash313@gmail.com>
2025-03-04 03:26:53 +0530

    add : initial content view design

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -9,13 +9,81 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Bg image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                // Pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                // Curreny exchange text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                
+                // Conversion section
+                HStack {
+                    // Left conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency Image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            // Currency text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        // Textfield
+                        Text("Textfield")
+                    }
+                    
+                    // Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                    // Right conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency Text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            // Currency Image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        
+                        // Textfield
+                        Text("Textfield")
+                    }
+                }
+                
+                Spacer()
+                
+                // Info button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+            }
         }
-        .padding()
     }
 }
 
