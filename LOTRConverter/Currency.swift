1506aa6470f7b359b80d74000c24acfb2672ba98
deepak <deepakkumardash313@gmail.com>
2025-03-06 02:42:24 +0530

    add : Curency enum and lazyvgrid

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Deepak kumar Dash on 06/03/25.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
